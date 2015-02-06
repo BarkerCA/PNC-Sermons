b7b62237518049a614e61be3f7d89a5f49899ccc
Chris Barker <chris.a.barker@icloud.com>
2015-02-05 23:06:07 -0500

    Apple Touche Icon

--- PHP/assets/includes/head.inc.php
+++ PHP/assets/includes/head.inc.php
@@ -3,6 +3,12 @@
   <head>
     <title>Pointe North Church :: <?php echo $pagetitle; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Pointe North Church Sermons">
+    <link rel="apple-touch-icon" href="http://assets.pointenorth.org/img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="http://assets.pointenorth.org/img/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="http://assets.pointenorth.org/img/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="http://assets.pointenorth.org/img/apple-touch-icon-152x152.png">
     <link rel="stylesheet" media="all" href="http://assets.pointenorth.org/css/sermons.css?v=1.0" data-turbolinks-track="true" />
     <link rel="Shortcut Icon" href="http://assets.pointenorth.org/img/favicon.ico" type="image/x-icon" />
     <script type="text/javascript" src="http://assets.pointenorth.org/libs/foundation-5.5.0/js/vendor/modernizr.js"></script>
