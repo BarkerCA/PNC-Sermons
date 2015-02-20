e331e3060e2094c03c980913210be1f534588539
Chris Barker <chris.a.barker@icloud.com>
2015-02-20 11:08:03 -0500

    Series.php Update

--- PHP/assets/includes/series.inc.php
+++ PHP/assets/includes/series.inc.php
@@ -10,14 +10,38 @@
 # sdate   = Start date of the sermon series
 # edate   = End date of the sermon series
 #
+#
+# Soundcloud Player Sizes
+#
+# 1 Sermon
+$single_player    = "125";
+# 2 Sermons
+$small_player     = "225";
+# 3-4 Sermons
+$medium_player    = "375";
+# 4-5 Sermons
+$large_player     = "450";
+# 6-10 Sermons
+$huge_player      = "600";
 
 
 $series = array(
 #
+# Commitment Single Sermon
+#
+  "commitment"              => array("image"  => "http://assets.pointenorth.org/img/sermons/PNC_Sermon.jpg",
+                                     "pheight"=> $single_player,
+                                     "title"  => "Commitment", 
+                                     "tracks" => 1, 
+                                     "url"    => "https://soundcloud.com/pointe-north/commitment-02-15-2014-timdorn",
+                                     "sdate"  => "February 15, 2015",
+                                     "edate"  => "February 15, 2015",
+                                    ),
+#
 # Glory Days Sermon Series
 #
   "glory-days"              => array("image"  => "http://assets.pointenorth.org/img/sermons/GloryDays.jpg",
-                                     "pheight"=> "450",
+                                     "pheight"=> $large_player,
                                      "title"  => "Glory Days", 
                                      "tracks" => 4, 
                                      "url"    => "https://soundcloud.com/pointe-north/sets/glory-days",
@@ -28,7 +52,7 @@ $series = array(
 # Welcome To The Family Sermon Series
 #
   "welcome-to-the-family"   => array("image"  => "http://assets.pointenorth.org/img/sermons/WelcomeToTheFamily.jpg",
-                                     "pheight"=> "375",
+                                     "pheight"=> $medium_player,
                                      "title"  => "Welcome To The Family", 
                                      "tracks" => 3, 
                                      "url"    => "https://soundcloud.com/pointe-north/sets/welcome-to-the-family",
@@ -39,7 +63,7 @@ $series = array(
 # Thanks-Giving Sermon Series
 #
   "thanks-giving"           => array("image"  => "http://assets.pointenorth.org/img/sermons/ThanksGiving.jpg",
-                                     "pheight"=> "450",
+                                     "pheight"=> $large_player,
                                      "title"  => "Thanks-Giving", 
                                      "tracks" => 5, 
                                      "url"    => "https://soundcloud.com/pointe-north/sets/thanks-giving",
